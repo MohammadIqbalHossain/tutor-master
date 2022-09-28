d8739b298e5df3528e4239d8b7b7284d18486440
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-09-29 01:31:09 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+         
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
